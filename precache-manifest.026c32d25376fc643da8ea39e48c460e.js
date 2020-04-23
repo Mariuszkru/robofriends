b99ce3b73bce486d33cc0d29544f69d740de8869
Mariusz Krukowski <mariusz.krukowski@aol.com>
2020-04-23 22:08:02 +0200

    Updates

---
+++ precache-manifest.026c32d25376fc643da8ea39e48c460e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9272bc203c41c5f2b017318c9dbb9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e58a7aafe791bc8ee25",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "e576ffda5ff1cba76a63",
+    "url": "/static/css/main.2cf75415.chunk.css"
+  },
+  {
+    "revision": "2e58a7aafe791bc8ee25",
+    "url": "/static/js/2.6940dd55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6940dd55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e576ffda5ff1cba76a63",
+    "url": "/static/js/main.034ed14d.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
